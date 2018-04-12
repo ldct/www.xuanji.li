57ab77a4d2cacf8f9c1dcb1b8644e40a8fda8c59
Xuanji Li <xuanji@gmail.com>
2018-04-12 22:39:00 +0800

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+git clone git@github.com:ldct/isicp.git
+cd isicp
+git clone git@github.com:yuanchenyang/coding-js.git
+./render_all.sh
+cd ..
+git clone git@github.com:ldct/parallel-text.git
+git clone git@github.com:ldct/js-scheme-stk.git
+git clone git@github.com:ldct/goodreads-wall.git
+git clone git@github.com:ldct/2048.git
+git clone git@github.com:ldct/card.js.git
+gcloud app deploy .
